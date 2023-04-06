76e339122452f847eccf2edd67726bc248cde548
CHHUONN.Sophorn <chhuonn.sophorn@ababank.com>
2023-04-06 13:14:22 +0700

    updated share handler

--- src/pages/BridgeHandlers/HandlerShareDownload.vue
+++ src/pages/BridgeHandlers/HandlerShareDownload.vue
@@ -21,7 +21,7 @@
 </template>
 
 <script setup>
-import { reactive } from "vue";
+import { reactive, inject } from "vue";
 const $bridge = inject("$bridge");
 const share = reactive({
   fileName: "",
@@ -29,7 +29,6 @@ const share = reactive({
   type: "text/plain",
   message: "Share Content",
 });
-
 function onSubmit() {
   $bridge.callHandler("share", [share]);
 }
