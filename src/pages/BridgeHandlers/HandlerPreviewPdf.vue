eb665d44c17b8faf08841ad59d7e728c03e3d096
CHHUONN.Sophorn <chhuonn.sophorn@ababank.com>
2025-12-01 15:32:08 +0700

    change preview PDF file

--- src/pages/BridgeHandlers/HandlerPreviewPdf.vue
+++ src/pages/BridgeHandlers/HandlerPreviewPdf.vue
@@ -19,6 +19,7 @@ const $bridge = inject("$bridge");
 
  
 const onSubmit = () => {
+  console.log('File size is 8.5MB')
   const payload = {
     fileName: "ABA Mobile TechStack.pdf",
     content: techStackPdfBase64,
